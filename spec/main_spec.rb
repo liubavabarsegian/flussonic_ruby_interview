3df751f4ce003cc10a89251ddfa7830522ada0d5
liubavabarsegian <lyubahemmo@mail.ru>
2023-03-09 17:57:48 +0300

    added add function and tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -70,4 +70,41 @@ RSpec.describe Chain do
         end
     end
   end
+
+  describe 'add' do
+    context 'add anually' do
+        it 'should add 2025' do
+            periods = Chain.new("16.07.2023", ["2023", "2024"])
+            new_period_type = "annually"
+            expect(periods.add(new_period_type)).to eq(["2023", "2024", "2025"])
+        end
+
+        it 'should add 2025' do
+            periods = Chain.new("31.12.2023", ["2023M12D31", "2024M1", "2024"])
+            new_period_type = "annually"
+            expect(periods.add(new_period_type)).to eq(["2023M12D31", "2024M1", "2024", "2025"])
+        end
+    
+    context 'add montly' do
+        it 'should add 2023M3' do
+            periods = Chain.new("30.01.2023", ["2023M1D30", "2023M1", "2023M2"])
+            new_period_type = "monthly"
+            expect(periods.add(new_period_type)).to eq(["2023M1D30", "2023M1", "2023M2", "2023M3"])
+        end
+    end
+
+    context 'add daily' do
+        it 'should add 2023M3D31' do
+            periods = Chain.new("30.01.2023", ["2023M1D30", "2023M1", "2023M2", "2023M3D30"])
+            new_period_type = "daily"
+            expect(periods.add(new_period_type)).to eq(["2023M1D30", "2023M1", "2023M2", "2023M3D30", "2023M3D31"])
+        end
+    end
+        it 'should add 2023M4D1' do
+            periods = Chain.new("30.01.2023", ["2023M1D30", "2023M1", "2023M2", "2023M3D31"])
+            new_period_type = "daily"
+            expect(periods.add(new_period_type)).to eq(["2023M1D30", "2023M1", "2023M2", "2023M3D31", "2023M4D1"])
+        end
+    end
+  end
 end
